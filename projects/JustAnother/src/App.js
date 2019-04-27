e410a8161c2fca3c3b3b8f5c7d2178ac780b4880
Autocommit <devnull@ati.su>
2019-04-27 18:26:04 +0300

    many changes

--- projects/JustAnother/src/App.js
+++ projects/JustAnother/src/App.js
@@ -1,110 +1,78 @@
-import React from 'react';
-import './style.css';
-import Character from './character.js';
-import ReactDOM from 'react-dom';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const fs = require("fs");
+require("./style.css");
+// FIXME: kill it all!!!
 class CharacterName extends React.Component {
-
-    ViewCharacter(){
-
-        function readTextFile(file, callback) {
-            var rawFile = new XMLHttpRequest();
-            rawFile.overrideMimeType("application/json");
-            rawFile.open("GET", file, true);
-            rawFile.onreadystatechange = function() {
-                if (rawFile.readyState === 4 && rawFile.status == "200") {
-                    callback(rawFile.responseText);
+    constructor() {
+        super(...arguments);
+        this.ViewCharacter = () => {
+            const readTextFile = (file, callback) => {
+                const rawFile = new XMLHttpRequest();
+                rawFile.overrideMimeType("application/json");
+                rawFile.open("GET", file, true);
+                rawFile.onreadystatechange = () => {
+                    if (rawFile.readyState === 4 && rawFile.status == 200) {
+                        callback(rawFile.responseText);
+                    }
+                };
+                rawFile.send();
+            };
+            let data = null;
+            const newWin = window.open('./character.html', 'example', 'width=600,height=400');
+            readTextFile("./characterInfo.json", (text) => {
+                data = JSON.parse(text);
+                let charName = "Wolf";
+                let char = data.characters.filter(function (item) { return item.name == charName; }); //data[charName].description;
+                /*let i = 0;
+                for (; i < data.characters.length; i++){
+                    if (data.characters[0].name == charName){
+                        break;
+                    }
                 }
-            }
-            rawFile.send();
-        }
-
-        let data = null;
-        let win = this.window;
-
-        let newWin = window.open('./character.html', 'example', 'width=600,height=400');
-
-
-
-        readTextFile("./characterInfo.json", function(text){
-            data = JSON.parse(text);
-
-            let charName = "Wolf";
-
-            let char = data.characters.filter( function(item) {return item.name == charName}  )//data[charName].description;
-
-            /*let i = 0;
-            for (; i < data.characters.length; i++){
-                if (data.characters[0].name == charName){
-                    break;
+    
+                console.log(i);*/
+                let index = data.characters.indexOf(char[0]);
+                if (char != null) {
+                    let charDescr = char[0].description;
+                    let charImg = char[0].img;
+                    CreateNewWindow(charName, charDescr, charImg);
+                    newWin.onbeforeunload = function () {
+                        let _name = newWin.document.getElementById('name');
+                        let _description = newWin.document.getElementById('description');
+                        let _photo = newWin.document.getElementById('photo');
+                        //let ind = data.indexOf(char[0]);
+                        char[0].name = "Chert"; //_name.innerText;
+                        char[0].description = _description.innerText;
+                        char[0].img = _photo.innerText;
+                        data.characters[index] = char[0];
+                        var _json = JSON.stringify(data);
+                        fs.writeFile("C:\test.txt", _json, function (err) { });
+                        //return [_name, _description, _photo];
+                    };
                 }
-            }
-
-            console.log(i);*/
-
-            let index = data.characters.indexOf(char[0]);
-
-            if (char != null) {
-                let charDescr = char[0].description;
-                let charImg = char[0].img;
-
-                CreateNewWindow(charName, charDescr, charImg);
-
-                newWin.onbeforeunload = function() {
+                //let charDescr = (data) => data.filter(item == charName);
+                //console.log(charDescr);
+                let charImg = null;
+                console.log(data);
+            });
+            function CreateNewWindow(charName, charDescr, charImg) {
+                newWin.onload = function () {
                     let _name = newWin.document.getElementById('name');
                     let _description = newWin.document.getElementById('description');
                     let _photo = newWin.document.getElementById('photo');
-
-                    //let ind = data.indexOf(char[0]);
-
-                    char[0].name = "Chert";//_name.innerText;
-                    char[0].description = _description.innerText;
-                    char[0].img = _photo.innerText;
-
-                    data.characters[index] = char[0];
-
-                    var _json = JSON.stringify(data);
-
-                    var fs = require('fs');
-                    fs.writeFile("C:\test.txt", _json, function(err) {});
-                    //return [_name, _description, _photo];
-                }
+                    _name.innerText = charName;
+                    _description.innerText = charDescr;
+                    _photo.innerText = charImg;
+                    //char.innerText = "charName";
+                };
             }
-
-            //let charDescr = (data) => data.filter(item == charName);
-            //console.log(charDescr);
-            let charImg = null;
-
-
-            console.log(data);
-        });
-
-        function CreateNewWindow(charName, charDescr, charImg) {
-
-            newWin.onload = function () {
-                let _name = newWin.document.getElementById('name');
-                let _description = newWin.document.getElementById('description');
-                let _photo = newWin.document.getElementById('photo');
-
-                _name.innerText = charName;
-                _description.innerText = charDescr;
-                _photo.innerText = charImg;
-                //char.innerText = "charName";
-            }
-
-        }
-
+        };
     }
-
     render() {
-        return (
-            <div className="parent">
-                <div className="child" onClick={() => this.ViewCharacter()}>
-                    Hello, just another react
-                </div>
-            </div>
-        );
+        return (React.createElement("div", { className: "parent" },
+            React.createElement("div", { className: "child", onClick: () => this.ViewCharacter() }, "Hello, just another react")));
     }
 }
-
-export default CharacterName;
+exports.default = CharacterName;
--- projects/JustAnother/src/Node/Node.tsx
+++ projects/JustAnother/src/Node/Node.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import "./Node.css"; 
 import Popup from "../Popup/Popup.js"
 import { observer } from "mobx-react" 
